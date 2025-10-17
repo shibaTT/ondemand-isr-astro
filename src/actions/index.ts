eecb50fee14f6aa6a56ffb6c5e4f8d7657c0b28c
shibaTT <39818834+shibaTT@users.noreply.github.com>
2025-10-17 14:59:15 +0900

    ページ追加とActionsの追加

---
+++ src/actions/index.ts
@@ -0,0 +1,29 @@
+import { defineAction } from "astro:actions";
+import { z } from "astro:schema";
+
+export const server = {
+    generatePage: defineAction({
+        handler: async () => {
+            // CloudflareにAPIを送ってページをパージする
+            const response = await fetch(
+                `https://api.cloudflare.com/client/v4/zones/${process.env.CLOUDFLARE_ZONE_ID}/purge_cache`,
+                {
+                    method: "POST",
+                    headers: {
+                        Authorization: `Bearer ${process.env.CLOUDFLARE_API_KEY}`,
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        files: ["https://ondemand-isr-astro.torisan.workers.dev"],
+                    }),
+                }
+            );
+
+            if (!response.ok) {
+                return false;
+            }
+
+            return true;
+        },
+    }),
+};
